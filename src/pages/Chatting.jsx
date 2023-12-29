e3530c716b4c762e4ee8c8321d3a7d3eac6f51cc
cbum-dev <priyanshukumar2002234@gmail.com>
2023-12-30 00:51:33 +0530

    finalised the personal chatting

--- src/pages/Chatting.jsx
+++ src/pages/Chatting.jsx
@@ -1,165 +1,182 @@
 import React, { useState, useEffect } from "react";
-import client, { databases, DATABASE_ID, COLLECTION_ID_MESSAGING, COLLECTION_ID_NEW } from "../appwriteConfig";
-import {ID, Query,Permission,Role } from "appwrite";
+import client, {
+  databases,
+  DATABASE_ID,
+  COLLECTION_ID_MESSAGING,
+  COLLECTION_ID_NEW,
+} from "../appwriteConfig";
+import { ID, Query, Permission, Role } from "appwrite";
 import { useParams } from "react-router-dom";
 import Header from "../components/Header";
 import { useAuth } from "../utils/AuthContext";
+import { Trash2 } from "react-feather";
 
 const Chatting = () => {
-    const [messageBody, setMessageBody] = useState('')
-    const [messag, setMessag] = useState([]);
-    const [neww, setNeww] = useState([]); // Added state for 'neww'
-    const { user } = useAuth();
-    const messageId = useParams()
-    let val = 0
-    let id = 0;
+  const [messageBody, setMessageBody] = useState("");
+  const [messag, setMessag] = useState([]);
+  const [neww, setNeww] = useState([]); // Added state for 'neww'
+  const { user } = useAuth();
+  const messageId = useParams();
+  let val = 0;
+  let id = 0;
 
-    useEffect(() => {
-        getNew(); // Call the function to fetch 'neww' data
+  useEffect(() => {
+    getNew(); // Call the function to fetch 'neww' data
 
-        const unsubscribe = client.subscribe(`databases.${DATABASE_ID}.collections.${COLLECTION_ID_MESSAGING}.documents`, response => {
-            // Handle real-time updates for COLLECTION_ID_MESSAGING if needed
-        });
+    const unsubscribe = client.subscribe(
+      `databases.${DATABASE_ID}.collections.${COLLECTION_ID_MESSAGING}.documents`,
+      (response) => {
+        // Handle real-time updates for COLLECTION_ID_MESSAGING if needed
+      }
+    );
 
-        console.log('unsubscribe:', unsubscribe);
+    console.log("unsubscribe:", unsubscribe);
 
-        return () => {
-            unsubscribe();
-        };
-    }, [user]);
-
-    const getNew = async () => {
-        try {
-            const response = await databases.listDocuments(
-                DATABASE_ID,
-                COLLECTION_ID_NEW,
-                [
-                    Query.equal('user',user.$id)
-                ]
-            );
-
-            console.log(response.documents);
-            setNeww(response.documents);
-            val = response.documents[0].$id;
-            id = response.documents[0].user;
-            // console.log(val.toString())
-            getMessag();
-
-            // console.log(response.documents[0].user)
-        } catch (error) {
-            console.error("Error fetching neww documents:", error);
-            // Handle errors as needed
-        }
+    return () => {
+      unsubscribe();
     };
+  }, [user]);
 
-    const getMessag = async () => {
-        try {
-            const response = await databases.listDocuments(
-                DATABASE_ID,
-                COLLECTION_ID_MESSAGING,
-                
-                [
-                    Query.equal('new', messageId.messageId),
-                    // Query.equal('new_by',[val]),
-                    Query.orderDesc('$createdAt')
-                    // Query.equal('new_by' , '6587e150103bc459133c')
-                ],
-                
+  const getNew = async () => {
+    try {
+      const response = await databases.listDocuments(
+        DATABASE_ID,
+        COLLECTION_ID_NEW,
+        [Query.equal("user", user.$id)]
+      );
 
-            );
-            // console.log(val)
-            if (user.$id===id) {
-            console.log(true)
-            console.log(id,user.$id)
-                
-            }
-            console.log(response.documents);
-            console.log(messageId.messageId)
-            setMessag(response.documents);
-        } catch (error) {
-            console.error("Error fetching messages:", error);
-        }
-    };
-    const handleSubmit = async (e) => {
-        e.preventDefault()
-        await getNew();
-        console.log('MESSAGE:', messageBody)
+      console.log(response.documents);
+      setNeww(response.documents);
+      val = response.documents[0].$id;
+      id = response.documents[0].user;
+      // console.log(val.toString())
+      getMessag();
 
-        const permissions = [
-            Permission.write(Role.user(user.$id)),
-          ]
-          const existingNewId = messageId.messageId;
-          const existingNewbyId = val;
-          console.log(existingNewbyId)
-          const payload = {
-            message: messageBody,
-            new: existingNewId,
-            new_by: existingNewbyId, 
-        };
-    
-
-        const response = await databases.createDocument(
-                DATABASE_ID, 
-                COLLECTION_ID_MESSAGING, 
-                ID.unique(), 
-                payload,
-                permissions
-            )
-
-        console.log('RESPONSE:', response)
-
-        // setMessages(prevState => [response, ...prevState])
-
-        setMessageBody('')
-
+      // console.log(response.documents[0].user)
+    } catch (error) {
+      console.error("Error fetching neww documents:", error);
+      // Handle errors as needed
     }
-    return (
-        <main className="container">
-            <Header/>
-            <div className="room--container">
-            <form id="message--form" onSubmit={handleSubmit}>
-            <div>
-                <textarea 
-                    required 
-                    maxLength="250"
-                    placeholder="Say something..." 
-                    onChange={(e) => {setMessageBody(e.target.value)}}
-                    value={messageBody}
-                    ></textarea>
-            </div>
+  };
 
-            <div className="send-btn--wrapper">
-                <input className="btn btn--secondary" type="submit" value="send"/>
-            </div>
+  const getMessag = async () => {
+    try {
+      const response = await databases.listDocuments(
+        DATABASE_ID,
+        COLLECTION_ID_MESSAGING,
+
+        [
+          Query.equal("new", messageId.messageId),
+          // Query.equal('new_by',[val]),
+          Query.orderDesc("$createdAt"),
+          // Query.equal('new_by' , '6587e150103bc459133c')
+        ]
+      );
+      // console.log(val)
+      if (user.$id === id) {
+        console.log(true);
+        console.log(id, user.$id);
+      }
+      console.log(response.documents);
+      console.log(messageId.messageId);
+      setMessag(response.documents);
+    } catch (error) {
+      console.error("Error fetching messages:", error);
+    }
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await getNew();
+    console.log("MESSAGE:", messageBody);
+
+    const permissions = [Permission.write(Role.user(user.$id))];
+    const existingNewId = messageId.messageId;
+    const existingNewbyId = val;
+    console.log(existingNewbyId);
+    const payload = {
+      message: messageBody,
+      new: existingNewId,
+      new_by: existingNewbyId,
+    };
+
+    const response = await databases.createDocument(
+      DATABASE_ID,
+      COLLECTION_ID_MESSAGING,
+      ID.unique(),
+      payload,
+      permissions
+    );
+
+    console.log("RESPONSE:", response);
+
+    // setMessages(prevState => [response, ...prevState])
+
+    setMessageBody("");
+  };
+  const handleDelete = async (id) => {
+    await databases.deleteDocument(DATABASE_ID, COLLECTION_ID_MESSAGING, id);
+  };
+  return (
+    <main className="container">
+      <Header />
+      <div className="room--container">
+        <form id="message--form" onSubmit={handleSubmit}>
+          {/* ... (other form elements) */}
+          <div>
+            <textarea
+              required
+              maxLength="250"
+              placeholder="Say something..."
+              onChange={(e) => {
+                setMessageBody(e.target.value);
+              }}
+              value={messageBody}
+            ></textarea>
+          </div>
+
+          <div className="send-btn--wrapper">
+            <input className="btn btn--secondary" type="submit" value="send" />
+          </div>
         </form>
-            <div>
-                {messag.map(message => (
-                    <div key={message.$id} className={"message--wrapper"}>
-                        <div className="message--header">
-                            <p> 
-                                {message?.user ? (
-                                    <span> {message.username}</span>
-                                ): (
-                                    'Anonymous user'
-                                )}
-                             
-                                <small className="message-timestamp"> {new Date(message.$createdAt).toLocaleString()}</small>
-                                {message.$id}
-                            </p>
-                        </div>
-    
-                        <div className={"message--body" + (id.toString() === user.$id.toString() ? ' message--body--owner' : '')}>
-                            <span>{message.message}</span>
-                            {/* <p>as</p> */}
-                            
-                        </div>                    </div>
-                        
-                ))}
+        <div>
+          {messag.map((message) => (
+            <div key={message.$id} className={"message--wrapper"}>
+              <div className="message--header">
+              <p>
+                {message?.new_by ? (
+                  <span> {message.new_by.username}</span>
+                ) : (
+                  "Anonymous user"
+                )}
+
+                <small className="message-timestamp">
+                  {" "}
+                  {new Date(message.$createdAt).toLocaleString()}
+                </small>
+              </p>
+              {message.new_by.user === user.$id && (
+                <Trash2
+                  className="delete--btn "
+                  onClick={() => handleDelete(message.$id)}
+                />
+              )}
+              </div>
+              <div
+                className={
+                  "message--body" +
+                  (message.new_by.user === user.$id
+                    ? " message--body--owner"
+                    : "")
+                }
+              >
+                <span>{message.message}</span>
+              </div>
             </div>
-            </div>
-            
-        </main>
-      )
+          ))}
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default Chatting;
